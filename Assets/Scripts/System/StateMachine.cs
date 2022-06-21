3f65df253c5ac8014373f08ad9968d745477e506
HarashimaYoshikazu <harashima318@gmail.com>
2022-06-21 13:57:18 +0900

    [fix]StateMachineクラスのコメントを削除

--- Assets/Scripts/System/StateMachine.cs
+++ Assets/Scripts/System/StateMachine.cs
@@ -8,51 +8,31 @@ public class StateMachine<Event> where Event : System.Enum
     /// </summary>
     public abstract class State
     {
-        /// <summary>
-        /// このステートを管理しているステートマシン
-        /// </summary>
         protected StateMachine<Event> StateMachine => stateMachine;
         internal StateMachine<Event> stateMachine;
-        /// <summary>
-        /// 遷移の一覧
-        /// </summary>
+
         internal Dictionary<Event, State> transitions = new Dictionary<Event, State>();
 
 
-        /// <summary>
-        /// ステート開始
-        /// </summary>
         internal void Enter(State prevState)
         {
             OnEnter(prevState);
         }
-        /// <summary>
-        /// ステートを開始した時に呼ばれる
-        /// </summary>
         protected virtual void OnEnter(State prevState) { }
 
-        /// <summary>
-        /// ステート更新
-        /// </summary>
+
         internal void Update()
         {
             OnUpdate();
         }
-        /// <summary>
-        /// 毎フレーム呼ばれる
-        /// </summary>
+
         protected virtual void OnUpdate() { }
 
-        /// <summary>
-        /// ステート終了
-        /// </summary>
         internal void Exit(State nextState)
         {
             OnExit(nextState);
         }
-        /// <summary>
-        /// ステートを終了した時に呼ばれる
-        /// </summary>
+
         protected virtual void OnExit(State nextState) { }
     }
 
@@ -70,9 +50,6 @@ public class StateMachine<Event> where Event : System.Enum
     private LinkedList<State> states = new LinkedList<State>();
 
 
-    /// <summary>
-    /// ステートを追加する（ジェネリック版）
-    /// </summary>
     public T Add<T>() where T : State, new()
     {
         var state = new T();
@@ -81,9 +58,7 @@ public class StateMachine<Event> where Event : System.Enum
         return state;
     }
 
-    /// <summary>
-    /// 特定のステートを取得、なければ生成する
-    /// </summary>
+
     public T GetOrAddState<T>() where T : State, new()
     {
         foreach (var state in states)
@@ -96,9 +71,6 @@ public class StateMachine<Event> where Event : System.Enum
         return Add<T>();
     }
 
-    /// <summary>
-    /// 遷移を定義する
-    /// </summary>
     /// <param name="eventId">イベントID</param>
     public void AddTransition<TFrom, TTo>(Event eventId)
         where TFrom : State, new()
@@ -116,45 +88,32 @@ public class StateMachine<Event> where Event : System.Enum
         from.transitions.Add(eventId, to);
     }
 
-    /// <summary>
-    /// どのステートからでも特定のステートへ遷移できるイベントを追加する
-    /// </summary>
-    /// <param name="eventId">イベントID</param>
+
     public void AddAnyTransition<TTo>(Event eventId) where TTo : State, new()
     {
         AddTransition<AnyState, TTo>(eventId);
     }
 
-    /// <summary>
-    /// ステートマシンの実行を開始する（ジェネリック版）
-    /// </summary>
+
     public void StartSetUp<TFirst>() where TFirst : State, new()
     {
         Start(GetOrAddState<TFirst>());
     }
 
-    /// <summary>
-    /// ステートマシンの実行を開始する
-    /// </summary>
-    /// <param name="firstState">起動時のステート</param>
-    /// <param name="param">パラメータ</param>
+
     public void Start(State firstState)
     {
         CurrentState = firstState;
         CurrentState.Enter(null);
     }
 
-    /// <summary>
-    /// ステートを更新する
-    /// </summary>
+
     public void Update()
     {
         CurrentState.Update();
     }
 
-    /// <summary>
-    /// イベントを発行する
-    /// </summary>
+
     /// <param name="eventId">イベントID</param>
     public void Dispatch(Event eventId)
     {
@@ -170,10 +129,6 @@ public class StateMachine<Event> where Event : System.Enum
         Change(to);
     }
 
-    /// <summary>
-    /// ステートを変更する
-    /// </summary>
-    /// <param name="nextState">遷移先のステート</param>
     private void Change(State nextState)
     {
         CurrentState.Exit(nextState);
