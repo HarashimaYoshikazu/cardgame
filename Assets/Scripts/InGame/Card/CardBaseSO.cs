d136f02977d848b4010a415594ed769b183b4f32
HarashimaYoshikazu <harashima318@gmail.com>
2022-06-24 19:06:32 +0900

    [update]ウィンドウに機能追加

--- Assets/Scripts/InGame/Card/CardBaseSO.cs
+++ Assets/Scripts/InGame/Card/CardBaseSO.cs
@@ -1,25 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System;
 
-[CreateAssetMenu]
+[CreateAssetMenu, Serializable]
 public class CardBaseSO : ScriptableObject
 {
     [SerializeField, Tooltip("カードの名前")]
     string _name;
-    public string Name => _name;
+    public string Name
+    {
+        get { return _name; }
+    #if UNITY_EDITOR
+        set { _name = value; }
+    #endif
+    }
 
     [SerializeField, Range(1, 2), Tooltip("技の数")]
     int _skillValue;
-    public int SkillValue => _skillValue;
+    public int SkillValue
+    {
+        get { return _skillValue; }
+    #if UNITY_EDITOR
+        set { _skillValue = Mathf.Clamp(value, 1, 2); }
+    #endif
+    }
 
     [SerializeField, Tooltip("カードの属性")]
     Elements _element;
-    public Elements Element => _element;
+    public Elements Element
+    {
+        get { return _element; }
+    #if UNITY_EDITOR
+        set { _element = value; }
+    #endif
+    }
 
     [SerializeField, Tooltip("カードのスプライト")]
     Sprite _sprite;
-    public Sprite Sprite => _sprite;
+    public Sprite Sprite
+    {
+        get { return _sprite; }
+    #if UNITY_EDITOR
+        set { _sprite = value; }
+    #endif
+    }
 }
 
 public enum Elements
--- Assets/Scripts/System/Editor/CardGameEditerWindow.cs
+++ Assets/Scripts/System/Editor/CardGameEditerWindow.cs
@@ -1,12 +1,91 @@
 using UnityEngine;
 using UnityEditor;
+using System.IO;
+using UnityEngine.UI;
 
 public class CardGameEditerWindow : EditorWindow
 {
-    [MenuItem("Editor/Sample")]
+
+    DefaultAsset _TargetFolder;
+    private CardBaseSO _sample;
+    [MenuItem("Editor/CardGame")]
     private static void Create()
     {
         // 生成
-        GetWindow<CardGameEditerWindow>("サンプル");
+        GetWindow<CardGameEditerWindow>("CardGameWindow");
+    }
+    private void OnGUI()
+    {
+        if (_sample == null)
+        {
+            _sample = ScriptableObject.CreateInstance<CardBaseSO>();
+        }
+
+        using (new GUILayout.HorizontalScope())
+        {
+            _sample.Name = EditorGUILayout.TextField("カードの名前", _sample.Name);            
+        }
+
+        using (new GUILayout.HorizontalScope())
+        {
+            _sample.SkillValue = EditorGUILayout.IntField("スキルの数", _sample.SkillValue);
+        }
+
+        using (new GUILayout.HorizontalScope())
+        {
+            _sample.Element = (Elements)EditorGUILayout.EnumFlagsField("属性",_sample.Element);
+        }
+
+        using (new GUILayout.HorizontalScope())
+        {
+            _sample.Sprite = EditorGUILayout.ObjectField("Sprite", _TargetFolder, typeof(Sprite), false) as Sprite;
+        }
+
+        using (new GUILayout.HorizontalScope())
+        {
+            // 書き込みボタン
+            if (GUILayout.Button("書き込み"))
+            {
+                Export();
+            }
+            if (GUILayout.Button("読み込み"))
+            {
+                Import();
+            }
+        }
+    }
+
+
+    private const string ASSET_PATH = "Assets/Resources/WindowCard";
+    private void Export()
+    {
+        // 新規の場合は作成
+        if (!AssetDatabase.Contains(_sample as UnityEngine.Object))
+        {
+            string directory = Path.GetDirectoryName(ASSET_PATH);
+            if (!Directory.Exists(directory))
+            {
+                Directory.CreateDirectory(directory);
+            }
+            // アセット作成
+            AssetDatabase.CreateAsset(_sample, ASSET_PATH);
+        }
+        // インスペクターから設定できないようにする
+        _sample.hideFlags = HideFlags.NotEditable;
+        // 更新通知
+        EditorUtility.SetDirty(_sample);
+        // 保存
+        AssetDatabase.SaveAssets();
+        // エディタを最新の状態にする
+        AssetDatabase.Refresh();
+    }
+
+    private void Import()
+    {
+        CardBaseSO sample = AssetDatabase.LoadAssetAtPath<CardBaseSO>(ASSET_PATH);
+        if (sample == null)
+            return;
+
+        _sample = sample;
     }
 }
