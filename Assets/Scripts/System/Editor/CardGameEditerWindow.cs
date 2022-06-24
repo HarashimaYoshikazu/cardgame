62ce777ee01fdedd72f7645122383502f29767de
HarashimaYoshikazu <harashima318@gmail.com>
2022-06-24 17:11:52 +0900

    [add]エディターウィンドウ用のクラスを作成

---
+++ Assets/Scripts/System/Editor/CardGameEditerWindow.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEditor;
+
+public class CardGameEditerWindow : EditorWindow
+{
+    [MenuItem("Editor/Sample")]
+    private static void Create()
+    {
+        // ¶¬
+        GetWindow<CardGameEditerWindow>("ƒTƒ“ƒvƒ‹");
+    }
+}
